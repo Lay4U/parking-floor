2c66df403c7f1593821b7bc45f4bd629a7a2e28d
Lay\lay <kmr345@pluxity.com>
2023-09-26 16:18:55 +0900

    Initial commit

---
+++ parking-floor/src/routes/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+    import { onMount } from 'svelte';
+    let startFloor = -20;
+    let endFloor = 20;
+    let selectedFloor = null;
+
+    function selectFloor(floor) {
+        selectedFloor = floor;
+        if (typeof window !== 'undefined') {
+            localStorage.setItem('selectedFloor', floor);
+        }
+    }
+
+    onMount(() => {
+        if (typeof window !== 'undefined') {
+            selectedFloor = localStorage.getItem('selectedFloor');
+        }
+    });
+</script>
+
+<div>
+  <label>
+    시작 층:
+    <select bind:value={startFloor}>
+      {#each Array(41) as _, i}
+        <option>{i - 20}</option>
+      {/each}
+    </select>
+  </label>
+  <label>
+    끝 층:
+    <select bind:value={endFloor}>
+      {#each Array(41) as _, i}
+        <option>{i - 20}</option>
+      {/each}
+    </select>
+  </label>
+</div>
+
+{#if selectedFloor !== null}
+  <div>
+    {#each Array(endFloor - startFloor + 1) as _, i (i)}
+      <button on:click={() => selectFloor(i + startFloor)}>
+        {i + startFloor > 0 ? `${i + startFloor} 층` : i + startFloor < 0 ? `B${Math.abs(i + startFloor)} 층` : '기타'}
+      </button>
+    {/each}
+  </div>
+{/if}
+
+<p>선택된 층: {selectedFloor > 0 ? `${selectedFloor} 층` : selectedFloor < 0 ? `B${Math.abs(selectedFloor)} 층` : '기타'}</p>
